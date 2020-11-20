c320d87ef2227a40ea809592baf879a83a70e259
Ptolemy Henson <ptolemy2002@gmail.com>
2020-11-20 22:40:01 +0000

    Added liquid data

--- tags.py
+++ tags.py
@@ -67,14 +67,26 @@ def genTag(file, packName, packId):
                 workingList.extend(genTag(f"{argString[1:]}.mctag", packName, packId))
                 print(f'continuing to load "{file}"')
             else:
-              with open(f".saved/tags/{t}/{argString[1:]}.txt", "r") as data:
-                for i in data:
-                  for i2 in i.split(","):
-                    i2 = i2.strip()
-                    if not ":" in i2:
-                      workingList.append("minecraft:" + i2)
-                    else:
-                      workingList.append(i2)
+              def getEntries(path):
+                result = []
+                with open(f".saved/tags/{t}/{path}.txt", "r") as data:
+                    for i in data:
+                      for i2 in i.split(","):
+                        i2 = i2.strip()
+                        if i2[0] == "#":
+                          if not ":" in i2:
+                            workingList.extend(getEntries(f"minecraft:{i2[1:]}"))
+                          else:
+                            workingList.extend(getEntries(i2[1:]))
+                        else:
+                          if not ":" in i2:
+                            result.append("minecraft:" + i2)
+                          else:
+                            result.append(i2)
+
+                return result
+
+              workingList.extend(getEntries(argString[1:]))
           elif os.path.exists(f".saved/tags/{t}/{argString[1:]}.txt"):
             def getEntries(path):
               result = []
