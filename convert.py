52ae7bc03884f14fc247eb48e7da8f809a931f78
Ptolemy Henson <ptolemy2002@gmail.com>
2020-11-21 18:09:28 +0000

    Imported all default item tags.

--- convert.py
+++ convert.py
@@ -6,5 +6,5 @@ def start():
   with open("convertcsv.csv", "r") as file:
     reader = csv.DictReader(file)
     for i in reader:
-      with open(f".saved/tags/blocks/minecraft:{i['Tag name']}", "w+") as file:
+      with open(f".saved/tags/items/minecraft:{i['Tag name']}", "w+") as file:
         file.write(i["Values"])
